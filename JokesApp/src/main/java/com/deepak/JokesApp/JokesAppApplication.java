a9b88ec5ba88aedeb51100e0db93a57610444845
Deepak Sriram <dsriram@griddynamics.com>
2024-12-09 16:36:51 +0530

    Initialized Spring Project. closes #1

---
+++ JokesApp/src/main/java/com/deepak/JokesApp/JokesAppApplication.java
@@ -0,0 +1,11 @@
+package com.deepak.JokesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JokesAppApplication.class, args);
+    }
+}
